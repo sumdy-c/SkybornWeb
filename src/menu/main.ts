36d996e8d18c387dab5b09f274265b402da39283
sumdy-c <sumdy@mail.ru>
2023-11-19 03:27:37 +0300

    del example text

--- src/menu/main.ts
+++ src/menu/main.ts
@@ -20,10 +20,7 @@ export default class Menu {
     }
 
     create() {
-        // This creates a basic Babylon Scene object (non-mesh)
         var scene = new BABYLON.Scene(this.engine);
-
-        // This creates and positions a free camera (non-mesh)
         const camera = new BABYLON.ArcRotateCamera(
             "camera1",
             0.53,
@@ -32,21 +29,13 @@ export default class Menu {
             new BABYLON.Vector3(0, 1, 0),
             scene
         );
-
-        // This targets the camera to scene origin
         camera.setTarget(BABYLON.Vector3.Zero());
-
-        // This attaches the camera to the canvas
         camera.attachControl(this.viewport, true);
-
-        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
         var light = new BABYLON.HemisphericLight(
             'light',
             new BABYLON.Vector3(0, 1, 0),
             scene,
         );
-
-        // Default intensity is 1. Let's dim the light a small amount
         light.intensity = 0.7;
 
         // Our built-in 'sphere' shape.
@@ -55,20 +44,12 @@ export default class Menu {
             { diameter: 2, segments: 32 },
             scene,
         );
-
-        // Move the sphere upward 1/2 its height
         sphere.position.y = 1;
-
-        // Our built-in 'ground' shape.
         BABYLON.MeshBuilder.CreateGround(
             'ground',
             { width: 6, height: 6 },
             scene,
         );
-
-        // if(confirm('TEST')) {
-        //     const sound = new BABYLON.Sound("sound", "../../public/asset/test.mp3", scene, null, { autoplay: true  });
-        // }
 
         return scene;
     }
